f636d55914cc79cd7837ea3bafc1ec074bda0523
jonaskaun <jonas.kaun@crpusd.org>
2025-11-20 18:09:23 +0000

    hmm

--- 2.2_PhraseSolver/PhraseSolver.java
+++ 2.2_PhraseSolver/PhraseSolver.java
@@ -1,33 +1,29 @@
+
 /*
  * Activity 2.2.2
  *
  *  The PhraseSolver class the PhraseSolverGame
  */
 import java.util.Scanner;
-  
-public class PhraseSolver
-{
+
+public class PhraseSolver {
   Player player1 = new Player("");
   Player player2 = new Player("");
-  
+
   Board game = new Board();
   boolean solved = false;
 
-  public PhraseSolver(){
+  public PhraseSolver() {
     player1 = null;
     player2 = null;
-    
-    
+
     solved = false;
 
   }
 
-  
+  /* your code here - constructor(s) */
 
-  /* your code here - constructor(s) */ 
-
-  public void play()
-  {
+  public void play() {
     boolean solved = false;
     int currentPlayer = 1;
     Scanner input = new Scanner(System.in);
@@ -35,140 +31,144 @@ public class PhraseSolver
 
     String guess = input.nextLine();
     System.out.println(guess);
-  
+
     String phrase = game.getPhrase();
     System.out.println(phrase);
     String phraseOne = "";
 
+    if (guess.length() == 1) {
+      System.out.println("1 letter");
+      for (int t = 0; t < phrase.length(); t += 1) {
+        String a = phrase.substring(t, t + 1);
 
+        if (a.equals(guess)) {
+          phraseOne += guess;
 
-    if (guess.length() == 1){
-        System.out.println("1 letter");
-        for (int t = 0;t<phrase.length();t+=1){
-          String a = phrase.substring(t,t+1);
-        
-          if (a.equals(guess)){
-            phraseOne += guess;
-            
-          }
-          else if (a.equals(" ") ){
-            phraseOne += " ";
-          }
-          else{
-            phraseOne += "_";
-          }
+        } else if (a.equals(" ")) {
+          phraseOne += " ";
+        } else {
+          phraseOne += "_";
         }
-        System.out.println(phraseOne);
-        System.out.println("");
-          
+      }
+      System.out.println(phraseOne);
+      System.out.println("");
+
+      while (solved != true) {
+        incrementer++;
+        if ((incrementer % 2) == 0) {
+          currentPlayer = 2;
+
+        } else {
+          currentPlayer = 1;
+        }
+
+        System.out.println("Player " + currentPlayer + ": input your guess");
+
+        String guess2 = input.nextLine();
+        System.out.println(guess2);
+
+        String phrase2 = "";
+
+        if (guess2.length() == 1) {
+          System.out.println("1 letter");
+          for (int t = 0; t < phraseOne.length(); t += 1) {
+            String a = phraseOne.substring(t, t + 1);
+
+            if (a.equals(guess2)) {
+              phrase2 += guess2;
+
+            } else if (a.equals(" ")) {
+              phrase2 += " ";
+            } else {
+              phrase2 += "_";
+            }
+          }
+          System.out.println(phrase2);
+          System.out.println("");
+
+        }
+
+        else {
+          System.out.println("full phrase");
+          if (guess.equals(phraseOne)) {
+            solved = true;
+            System.out.println("you did it");
+          } else {
+            System.out.println("incorrect");
+          }
+
+        }
 
       }
-        
-      
+
+    }
+
     else {
       System.out.println("full phrase");
-      if (guess.equals(phrase)){
+      if (guess.equals(phrase)) {
         solved = true;
         System.out.println("you did it");
-      }
-      else{
+      } else {
         System.out.println("incorrect");
-      }
-    
-    
-    
 
+        while (solved != true) {
+          incrementer++;
+          if ((incrementer % 2) == 0) {
+            currentPlayer = 2;
 
-      
-
-    
-
-    
-  
-      
-      
-     
-      
-      
-      
-
-
-        
-
-
-
-
-      
-    while(solved == false){
-      incrementer++;
-      if ((incrementer%2) == 0){
-        currentPlayer = 2;
-        
-      }
-      else {
-        currentPlayer = 1;
-      }
-      
-      
-      
-      System.out.println("Player "+currentPlayer+ ": input your guess");
-      
-      
-      String guess2 = input.nextLine();
-      System.out.println(guess2);
-  
-        
-      String phrase2 = "";
-      
-      
-      if (guess2.length() == 1){
-        System.out.println("1 letter");
-        for (int t = 0;t<phraseOne.length();t+=1){
-          String a = phraseOne.substring(t,t+1);
-        
-          if (a.equals(guess2)){
-            phrase2 += guess2;
-            
+          } else {
+            currentPlayer = 1;
           }
-          else if (a.equals(" ") ){
-            phrase2 += " ";
-          }
-          else{
-            phrase2 += "_";
-          }
-        }
-        System.out.println(phrase2);
-        System.out.println("");
-          
 
-      }
-        
-      
-      else {
-        System.out.println("full phrase");
-        if (guess.equals(phraseOne)){
-          solved = true;
-          System.out.println("you did it");
-        }
-        else{
-          System.out.println("incorrect");
-        }
+          System.out.println("Player " + currentPlayer + ": input your guess");
 
+          String guess2 = input.nextLine();
+          System.out.println(guess2);
+
+          String phrase2 = "";
+
+          if (guess2.length() == 1) {
+            System.out.println("1 letter");
+            for (int t = 0; t < phraseOne.length(); t += 1) {
+              String a = phraseOne.substring(t, t + 1);
+
+              if (a.equals(guess2)) {
+                phrase2 += guess2;
+
+              } else if (a.equals(" ")) {
+                phrase2 += " ";
+              } else {
+                phrase2 += "_";
+              }
+            }
+            System.out.println(phrase2);
+            System.out.println("");
+
+          }
+
+          else {
+            System.out.println("full phrase");
+            if (guess.equals(phraseOne)) {
+              solved = true;
+              System.out.println("you did it");
+            } else {
+              System.out.println("incorrect");
+            }
+
+          }
+
+        }
       }
-      
-      
-    }
-    input.close();
+
+      input.close();
+      System.out.println("ur cooking");
       /* your code here - game logic */
-      
-      
-      /* your code here - determine how game ends */
-      
-    } 
 
-   solved = true; 
+      /* your code here - determine how game ends */
+
+    }
+
+    solved = true;
   }
-  
-  
+
 }
-  
